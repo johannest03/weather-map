b0911fe438fa5f636e392bc5b608671c3c500369
hannest03 <hannesasd@gmail.com>
2022-01-27 09:09:56 +0100

    feat: added station interface

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { Map } from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
+import { Station } from '../shared/station';
 
 const lat = 46.4892313;
 const lng = 11.3121383;
@@ -35,6 +36,28 @@ export class MapComponent implements OnInit {
     });
 
     this.map.addControl(new mapboxgl.NavigationControl());
+
+    for(let i = 0; i < 100; i++){
+      new mapboxgl.Marker({
+        color: "#F00",
+      })
+      .setLngLat(this.randomPosition())
+      .setPopup(new mapboxgl.Popup().setHTML("<h1>Hello World!</h1>"))
+      .addTo(this.map);
+    }
+  }
+
+  private randomPosition(): mapboxgl.LngLatLike{
+    let ret: mapboxgl.LngLatLike = { lng: 0, lat: 0 };
+
+    ret.lat = this.randomNumberBetween(46.983155152961736, 46.19563031598852);
+    ret.lng = this.randomNumberBetween(12.37097000960092, 10.407948265318456);
+
+    return ret;
+  }
+
+  private randomNumberBetween(max: number, min: number): number{
+    return Math.random() * (max - min) + min;
   }
 
 }
---
+++ src/app/shared/station.ts
@@ -0,0 +1,36 @@
+import { LngLatLike } from "mapbox-gl";
+
+class Position{
+    latitude: number = 0;
+    longitude: number = 0;
+    toLngLatLike(): LngLatLike{
+        return { lng: this.longitude, lat: this.latitude };
+    }
+}
+
+export interface Station {
+  id: string;
+  name: string;
+  lastUpdated: string;
+  position: Position;
+  altitude: number;
+  categoryId: number;
+  code: string;
+  dd: string;
+  ff: string;
+  hs: string;
+  n: string;
+  p: string;
+  q: string;
+  rh: string;
+  t: string;
+  w: string;
+  sd: string;
+  measurements: Measurement[];
+}
+
+interface Measurement {
+  code: string;
+  description: string;
+  imageUrl: string;
+}
